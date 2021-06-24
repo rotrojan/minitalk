61b0d33a2d55c7fea3ffef9b7cd632a5e6180db3
rotrojan <rotrojan@student.42.fr>
2021-06-24 13:16:26 +0200

    WIP

--- srcs/server.c
+++ srcs/server.c
@@ -6,28 +6,32 @@
 /*   By: rotrojan <rotrojan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 13:48:50 by rotrojan          #+#    #+#             */
-/*   Updated: 2021/06/23 20:25:20 by rotrojan         ###   ########.fr       */
+/*   Updated: 2021/06/24 12:24:53 by bigo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+char g_buffer[4096] = {0};
+
 static void	handler(int signal_num)
 {
 	static unsigned char	mask = 0b10000000;
-	static unsigned char	byte = 0;
+	static int				i = 0;
 
 	if (signal_num == SIGUSR1)
-		byte += mask;
+		g_buffer[i] += mask;
 	mask = mask >> 1;
 	if (mask == 0)
 	{
 		if (byte != '\0')
+		{
 			write(STDOUT_FILENO, &byte, 1);
 		else
 			ft_putstr_fd("\nMessage received\n", STDOUT_FILENO);
 		mask = 0b10000000;
 		byte = 0;
+		pause();
 	}
 }
 
@@ -50,7 +54,7 @@ int	main(int ac, char **av)
 	ft_putstr_fd("\n", STDOUT_FILENO);
 	while (1)
 	{
-		pause();
+		/* pause(); */
 	}
 	return (0);
 }
